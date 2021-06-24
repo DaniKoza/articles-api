38c08907563cc000bcab904e453395a1ee2ec9af
Dani Koza <danikoza@gmail.com>
2021-06-24 03:20:56 +0300

    added image upload support for articles + signup method with password hashing and email validation

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,7 +1,41 @@
+const mongoose = require('mongoose');
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+
 module.exports = {
     signup: (req, res) => {
-        res.status(200).json({
-            message : 'signup'
+        const { email, password } = req.body;
+
+        
+
+        User.find({email}).then((users) => {
+            if (users.length >= 1) {
+                return res.status(409).json({
+                    message: 'Email allready exists!'
+                })
+            }
+            bcrypt.hash(password, 10, (error, hash) => {
+                if (error) {
+                    return res.status(500).json({
+                        error
+                    })
+                }
+                const user = new User({
+                    _id: new mongoose.Types.ObjectId(),
+                    email,
+                    password: hash
+                });
+                user.save().then((result) => {
+                    console.log(result);
+                    res.status(200).json({
+                        message: 'User created'
+                    });
+                }).catch(error => {
+                    res.status(500).json({
+                        error
+                    })
+                });
+            });
         });
     },
     login: (req, res) => {
